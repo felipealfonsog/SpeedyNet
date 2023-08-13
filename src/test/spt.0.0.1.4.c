5785ca3159b8b7e15739877494166a56519ec704
Felipe <f.alfonso@res-ear.ch>
2023-08-13 07:06:33 -0400

    Updates 0.0.2

--- src/test/spt.0.0.1.4.c
+++ src/test/spt.0.0.1.4.c
@@ -9,14 +9,9 @@
 #include <sys/time.h>
 #include <curl/curl.h>
 #include <math.h>
-#include <netinet/ip_icmp.h>
-#include <signal.h>
 
 #define BUFFER_SIZE 1024
 #define TEST_DURATION 5
-
-int sockfd;
-int packet_loss_received = 0;
 
 void error(const char *msg) {
     perror(msg);
@@ -29,20 +24,10 @@ size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
 
 void displayIntro() {
     printf("\n");
-    printf("╔══╗──────╔╗─╔═╦╗─╔╗\n");
-    printf("║══╬═╦═╦═╦╝╠╦╣║║╠═╣╚╗\n");
-    printf("╠══║╬║╩╣╩╣╬║║║║║║╩╣╔╣\n");
-    printf("╚══╣╔╩═╩═╩═╬╗╠╩═╩═╩═╝\n");
-    printf("───╚╝──────╚═╝\n");
     printf("************************************\n");
     printf("*           SpeedyNet              *\n");
     printf("*   Internet Speed Test Utility    *\n");
     printf("************************************\n");
-    printf("\n");
-    printf("Version: v.0.0.2 - with love from Chile\n");
-    printf("Developed by Felipe Alfonso González - Computer Science Engineer\n");
-    printf("Contact: f.alfonso@res-ear.ch\n");
-    printf("GitHub: github.com/felipealfonsog\n");
     printf("\n");
 }
 
@@ -52,29 +37,6 @@ void displayLicense() {
     printf("\n");
 }
 
-unsigned short in_cksum(unsigned short *addr, int len) {
-    int nleft = len;
-    int sum = 0;
-    unsigned short *w = addr;
-    unsigned short answer = 0;
-
-    while (nleft > 1) {
-        sum += *w++;
-        nleft -= 2;
-    }
-
-    if (nleft == 1) {
-        *(unsigned char *)(&answer) = *(unsigned char *)w;
-        sum += answer;
-    }
-
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum >> 16);
-    answer = ~sum;
-
-    return answer;
-}
-
 double calculateLatency(const char *server) {
     struct timeval start_time, end_time;
     int sockfd;
@@ -82,12 +44,12 @@ double calculateLatency(const char *server) {
     char buffer[BUFFER_SIZE];
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("Error opening socket");
+    if (sockfd < 0) exit(1);
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(80);
-    if (inet_pton(AF_INET, server, &server_addr.sin_addr) <= 0) error("Invalid address");
+    if (inet_pton(AF_INET, server, &server_addr.sin_addr) <= 0) exit(1);
 
     gettimeofday(&start_time, NULL);
 
@@ -106,69 +68,10 @@ double calculateLatency(const char *server) {
     return latency;
 }
 
-void sigIntHandler(int signo) {
-    packet_loss_received++;
-    if (packet_loss_received >= 3) {
-        printf("\n\nPacket loss test completed.\n");
-        displayLicense();
-        close(sockfd);
-        exit(0);
-    }
-}
-
 double calculatePacketLoss(const char *server) {
-    struct sockaddr_in server_addr;
-    int sockfd;
-    int seq_num = 1;
-    int ttl = 64;  // Time-to-Live (TTL) value for the packet
-
-    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (sockfd < 0) error("Error opening socket");
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    if (inet_pton(AF_INET, server, &server_addr.sin_addr) <= 0) error("Invalid address");
-
-    // Set TTL
-    if (setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
-        perror("Error setting TTL");
-        close(sockfd);
-        exit(1);
-    }
-
-    // Install the signal handler for SIGINT
-    signal(SIGINT, sigIntHandler);
-
-    while (1) {
-        struct icmphdr icmp_header;
-        memset(&icmp_header, 0, sizeof(icmp_header));
-
-        icmp_header.type = ICMP_ECHO;
-        icmp_header.code = 0;
-        icmp_header.un.echo.id = getpid();
-        icmp_header.un.echo.sequence = seq_num++;
-
-        icmp_header.checksum = 0;
-        icmp_header.checksum = in_cksum((unsigned short *)&icmp_header, sizeof(icmp_header));
-
-        struct timeval start_time, end_time;
-        gettimeofday(&start_time, NULL);
-
-        sendto(sockfd, &icmp_header, sizeof(icmp_header), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-        char recv_buffer[BUFFER_SIZE];
-        ssize_t num_bytes = recv(sockfd, recv_buffer, sizeof(recv_buffer), 0);
-        if (num_bytes > 0) {
-            gettimeofday(&end_time, NULL);
-
-            if (end_time.tv_sec - start_time.tv_sec < 1) {
-                packet_loss_received--;
-            }
-        }
-        usleep(1000000);  // Wait for 1 second
-    }
-
-    return 0.0;  // Return 0.0 for demonstration purposes
+    // Simulate packet loss calculation
+    // Replace this with actual implementation
+    return 0.5; // 50% packet loss for demonstration purposes
 }
 
 int main() {
@@ -198,12 +101,12 @@ int main() {
     const char *selected_server = num_servers > 0 ? server_list[rand() % num_servers] : "8.8.8.8";
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("Error opening socket");
+    if (sockfd < 0) exit(1);
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(80);
-    if (inet_pton(AF_INET, selected_server, &server_addr.sin_addr) <= 0) error("Invalid address");
+    if (inet_pton(AF_INET, selected_server, &server_addr.sin_addr) <= 0) exit(1);
 
     int connect_result = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     int connection_failed = 0;
