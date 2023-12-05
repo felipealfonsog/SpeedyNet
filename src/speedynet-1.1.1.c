779d57329d9974195a01d93063721aadc2e7f51f
Felipe <f.alfonso@res-ear.ch>
2023-12-05 06:10:20 -0300

    Updates

--- src/speedynet-1.1.1.c
+++ src/speedynet-1.1.1.c
@@ -6,8 +6,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <time.h>
-#include <sys/time.h>
-#include <math.h>
 #include <signal.h>
 
 #define BUFFER_SIZE 1024
@@ -15,11 +13,7 @@
 #define MAX_CONNECT_RETRIES 5
 
 int sockfd;
-
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
-}
+int interrupted = 0;
 
 void cleanup() {
     if (sockfd >= 0) {
@@ -27,94 +21,27 @@ void cleanup() {
     }
 }
 
-void displayIntro() {
-    printf("\n");
-    printf("╔══╗──────╔╗─╔═╦╗─╔╗\n");
-    printf("║══╬═╦═╦═╦╝╠╦╣║║╠═╣╚╗\n");
-    printf("╠══║╬║╩╣╩╣╬║║║║║║╩╣╔╣\n");
-    printf("╚══╣╔╩═╩═╩═╬╗╠╩═╩═╩═╝\n");
-    printf("───╚╝──────╚═╝\n");
-    printf("************************************\n");
-    printf("*           SpeedyNet              *\n");
-    printf("*   Internet Speed Test Utility    *\n");
-    printf("************************************\n");
-    printf("\n");
-    printf("Version: v.0.0.2 - with love from Chile\n");
-    printf("Developed by Felipe Alfonso González - Computer Science Engineer\n");
-    printf("Contact: f.alfonso@res-ear.ch\n");
-    printf("GitHub: github.com/felipealfonsog\n");
-    printf("\n");
-}
-
-void displayLicense() {
-    printf("\n");
-    printf("This software is licensed under the MIT License.\n");
-    printf("\n");
-}
-
-int interrupted = 0;
-
 void sigIntHandler(int signo) {
     interrupted = 1;
 }
 
-int main() {
-    int sockfd;
+void displayIntro() {
+    // (Código de presentación)
+}
+
+void displayLicense() {
+    // (Código de licencia)
+}
+
+int performSpeedTest(const char *selected_server) {
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
     clock_t start_time, end_time;
     double total_time;
 
-    const char *server_list[] = {
-        "8.8.8.8",   // Google DNS
-        "1.1.1.1",   // Cloudflare DNS,
-    };
-    int num_servers = sizeof(server_list) / sizeof(server_list[0]);
-
-    srand(time(NULL));
-    const char *selected_server = NULL;
-
-    double latency = 0.0;
-    double packet_loss = 0.0;
-
-    int connection_failed = 0;
-    for (int i = 0; i < num_servers; i++) {
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0) {
-            printf("Error opening socket for speed test.\n");
-            return 1;
-        }
-
-        memset(&server_addr, 0, sizeof(server_addr));
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(80);
-        server_addr.sin_addr.s_addr = inet_addr(server_list[i]);
-
-        int connect_retries = 0;
-        while (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-            if (connect_retries >= MAX_CONNECT_RETRIES) {
-                connection_failed = 1;
-                break;
-            }
-            connect_retries++;
-            usleep(1000000);  // Wait for 1 second before retrying
-        }
-
-        if (!connection_failed) {
-            selected_server = server_list[i];
-            break;
-        }
-
-        close(sockfd);
-    }
-
-    if (connection_failed) {
-        selected_server = "8.8.8.8";  // Use a default server if all options failed
-    }
-
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        printf("Error opening socket for speed test.\n");
+        perror("Error opening socket for speed test");
         return 1;
     }
 
@@ -123,16 +50,15 @@ int main() {
     server_addr.sin_port = htons(80);
     server_addr.sin_addr.s_addr = inet_addr(selected_server);
 
-    int connect_result = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (connect_result < 0) {
-        printf("Error connecting to server for speed test.\n");
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error connecting to server for speed test");
+        cleanup();
         return 1;
     }
 
     start_time = clock();
     for (int i = 0; i < BUFFER_SIZE; i++) buffer[i] = 'a';
 
-    displayIntro();
     printf("Performing speed test...\n");
 
     time_t start_timestamp = time(NULL);
@@ -146,8 +72,8 @@ int main() {
         int recv_result = recv(sockfd, buffer, sizeof(buffer), 0);
 
         if (send_result < 0 || recv_result < 0) {
-            printf("Error sending/receiving data during speed test.\n");
-            close(sockfd);
+            perror("Error sending/receiving data during speed test");
+            cleanup();
             return 1;
         }
 
@@ -176,16 +102,56 @@ int main() {
     printf("Download Speed: %.2f Mbps\n", download_speed);
     printf("Upload Speed: %.2f Mbps\n", upload_speed);
 
-    if (connection_failed) {
-        printf("Connection Status: Failed\n");
-    } else {
-        printf("Latency: %.2f ms\n", latency);
-        printf("Packet Loss: %.2f%%\n", packet_loss * 100.0);
-    }
-
-    displayLicense();
-
     cleanup();
 
     return 0;
+}
+
+int main() {
+    const char *server_list[] = {
+        "8.8.8.8",   // Google DNS
+        "1.1.1.1",   // Cloudflare DNS,
+    };
+    int num_servers = sizeof(server_list) / sizeof(server_list[0]);
+
+    srand(time(NULL));
+    const char *selected_server = NULL;
+
+    int connection_failed = 0;
+    for (int i = 0; i < num_servers; i++) {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            perror("Error opening socket for speed test");
+            return 1;
+        }
+
+        struct sockaddr_in server_addr;
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(80);
+        server_addr.sin_addr.s_addr = inet_addr(server_list[i]);
+
+        int connect_retries = 0;
+        while (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+            if (connect_retries >= MAX_CONNECT_RETRIES) {
+                connection_failed = 1;
+                break;
+            }
+            connect_retries++;
+            usleep(1000000);  // Wait for 1 second before retrying
+        }
+
+        if (!connection_failed) {
+            selected_server = server_list[i];
+            break;
+        }
+
+        close(sockfd);
+    }
+
+    if (connection_failed) {
+        selected_server = "8.8.8.8";  // Use a default server if all options failed
+    }
+
+    return performSpeedTest(selected_server);
 }
