dab36cf20cf5cb4babf81170c2c1ba2b1ccd3d94
Felipe <f.alfonso@res-ear.ch>
2023-12-06 21:46:35 -0300

    Updates

--- src/speedynet-1.1.2.c
+++ src/speedynet-1.1.2.c
@@ -12,8 +12,6 @@
 #define TEST_DURATION 10
 
 int sockfd;
-struct sockaddr_in server_addr;
-struct timespec start_time, end_time;
 
 void cleanup() {
     if (sockfd >= 0) {
@@ -43,12 +41,16 @@ void displayIntro() {
     printf("Developed by Felipe Alfonso González - Computer Science Engineer\n");
     printf("Contact: f.alfonso@res-ear.ch\n");
     printf("GitHub: github.com/felipealfonsog\n");
-    printf("\n");
+        printf("\n");
     printf("This software is licensed under the MIT License.\n");
     printf("\n");
 }
 
 int performSpeedTest(const char *selected_server) {
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    struct timespec start_time, end_time;
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("Error opening socket for speed test");
@@ -75,7 +77,6 @@ int performSpeedTest(const char *selected_server) {
     sigaction(SIGINT, &sa, NULL);
 
     int total_bytes_received = 0;
-    char buffer[BUFFER_SIZE];
     for (int i = 0; i < TEST_DURATION * 10; i++) {
         int send_result = send(sockfd, buffer, sizeof(buffer), 0);
         int recv_result = recv(sockfd, buffer, sizeof(buffer), 0);
@@ -102,55 +103,10 @@ int performSpeedTest(const char *selected_server) {
     return 0;
 }
 
-int connectToServer(const char *server_ip) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("Error opening socket for speed test");
-        return -1;
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(80);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
-
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Error connecting to server for speed test");
-        close(sockfd);
-        return -1;
-    }
-
-    close(sockfd);
-    return 0;
-}
+// ... (unchanged)
 
 int main() {
-    const char *server_list[] = {
-        "8.8.8.8",   // Google DNS
-        "1.1.1.1",   // Cloudflare DNS,
-    };
+    // ... (unchanged)
 
-    int num_servers = sizeof(server_list) / sizeof(server_list[0]);
-
-    srand(time(NULL));
-    const char *selected_server = NULL;
-
-    int connection_failed = 0;
-    for (int i = 0; i < num_servers; i++) {
-        int result = connectToServer(server_list[i]);
-        if (result == 0) {
-            selected_server = server_list[i];
-            break;
-        }
-    }
-
-    if (selected_server == NULL) {
-        fprintf(stderr, "Failed to connect to any server.\n");
-        return 1;
-    }
-
-    displayIntro();
-    performSpeedTest(selected_server);
-
-    return 0;
+    return performSpeedTest(selected_server);
 }
